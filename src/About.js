876cc5f6628b441eaaf93cac003db2ecba606005
Cristian Jimenez <cjimenez6960@gmail.com>
2022-09-18 22:56:16 -0700

    Commiting files

---
+++ src/About.js
@@ -0,0 +1,18 @@
+const About = () => {
+    return ( 
+        <div className="about">
+            <h2 className="about-title">About</h2>
+            <div className="about-section">
+                <div className="about-text">
+                    <p>Born and raised in the Bay Area. I taught myself to code at the age of 17 years old.
+                        My goal is to get access into the blockchain space due to the potential that blockchains have
+                        I hope to gain experience working as a front-end, and back-end web developer.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+     );
+}
+ 
+export default About;
